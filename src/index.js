f460cddd668e96461dbd6321521176fa84947347
Gasser <gaserkhaled@yahoo.com>
2020-08-23 12:04:03 -0400

    fixed base path

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/https://gasser707.github.io/">
             <App />
         </BrowserRouter>
     </Provider>
